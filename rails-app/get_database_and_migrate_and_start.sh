ce16b963ccfd93546d2ebd7b03559085e26a046c
Sandro Räss <s.raess@me.com>
2024-03-25 09:25:20 +0100

    Update get_database_and_migrate_and_start.sh.

--- rails-app/get_database_and_migrate_and_start.sh
+++ rails-app/get_database_and_migrate_and_start.sh
@@ -2,9 +2,10 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 bundle exec rails db:migrate
-pg_restore --verbose --clean --no-acl --no-owner -d postgres://my_user:ILoveDoingKnowBe4Training!@db:5432/myappdb latest.dump
-#postgres://db:5432
-#myappdb
-echo "after pg_restore"
+
+POSTGRES_URL="postgres://${DB_USERNAME}:${DB_PASSWORD}@db:5432/${DB_NAME}"
+
+pg_restore --verbose --clean --no-acl --no-owner -d $POSTGRES_URL latest.dump
+
 bundle exec rails server -b 0.0.0.0
 #CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
